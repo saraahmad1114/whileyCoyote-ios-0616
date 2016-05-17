70058a0dc4e8fef13f8ebad6b7c467a0bee7f040
saraahmad1114 <saraahmad1114@yahoo.com>
2016-05-17 10:41:35 -0400

    coyote

--- WhileyCoyote/FISAppDelegate.m
+++ WhileyCoyote/FISAppDelegate.m
@@ -10,6 +10,24 @@
      * Write your loop here!
     
      */
+    NSUInteger steps = 0;
+    NSUInteger anvil = arc4random_uniform(25) + 26;
+    do{
+            NSLog(@"Meep! Meep!");
+                steps++;
+        }
+    while (steps <= anvil);
+    {
+        if (steps % 10 == 0 && steps > 0){
+            NSLog(@"YOU'RE CUCKOO!");
+        }
+        if (steps == anvil){
+            NSLog(@"SMASH!");
+        }
+    }
+    
+    
+
     
     // do not alter
     return YES;  //
